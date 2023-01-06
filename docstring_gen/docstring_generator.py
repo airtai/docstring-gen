3daee833b12a5d32a4b431704eedc867157cdf52
Harish Mohan Raj <harish@airt.ai>
2023-01-06 06:40:29 +0530

    Fix lineno issue in Python 3.7

--- docstring_gen/docstring_generator.py
+++ docstring_gen/docstring_generator.py
@@ -4,9 +4,11 @@
 __all__ = ['add_docstring_to_notebook']
 
 # %% ../nbs/Docstring_Generator.ipynb 2
-from typing import *
 import ast
+import tokenize
+from typing import *
 from pathlib import Path
+from io import BytesIO
 
 import nbformat
 
@@ -93,6 +95,51 @@ def _calculate_end_lineno(source: str, start_line_no: int) -> int:
     return ret_val - 1
 
 # %% ../nbs/Docstring_Generator.ipynb 12
+def _line_has_class_or_method(source: str, lineno: int) -> bool:
+    """Check if a line in the source code contains a class or method definition.
+
+    Args:
+        source: The source code as a string.
+        lineno: The line number to check.
+
+    Returns:
+        True if the line contains a class or method definition, False otherwise.
+    """
+    line = "".join(source.split("\n")[lineno - 1])
+    tokens = list(tokenize.tokenize(BytesIO(line.encode("utf-8")).readline))
+    return tokens[1].type == tokenize.NAME and tokens[1].string in {
+        "class",
+        "def",
+        "async",
+    }
+
+
+def _get_start_line_for_class_or_func(source: str, lineno: int) -> int:
+    """Get the line number of the first line containing a class or function definition.
+
+    Args:
+        source: The source code as a string.
+        lineno: The line number to start from.
+
+    Returns:
+        The line number of the first line containing a class or function definition,
+        or the original line number if no such line is found.
+    """
+    if _line_has_class_or_method(source, lineno):
+        return lineno
+
+    original_lineno = lineno
+    total_lines = source.split("\n")[lineno - 1 :]
+
+    for i in total_lines:
+        lineno += 1
+        if lineno > len(total_lines):
+            break
+        if _line_has_class_or_method(source, lineno):
+            return lineno
+    return original_lineno
+
+# %% ../nbs/Docstring_Generator.ipynb 14
 def _add_docstring(
     source: str,
     node: Union[ast.ClassDef, ast.FunctionDef, ast.AsyncFunctionDef],
@@ -112,6 +159,10 @@ def _add_docstring(
     """
     line_no = node.lineno + line_offset
 
+    # Fix for ast's node.lineno giving line number of decorator
+    # instead of function/class definition in Python 3.7
+    lino_no = _get_start_line_for_class_or_func(source, line_no)
+
     if hasattr(node, "end_lineno") and node.end_lineno is not None:
         end_line_no = node.end_lineno + line_offset
     else:
@@ -124,7 +175,7 @@ def _add_docstring(
     line_offset += len(docstring.split("\n"))
     return source, line_offset
 
-# %% ../nbs/Docstring_Generator.ipynb 14
+# %% ../nbs/Docstring_Generator.ipynb 16
 def _check_and_add_docstrings_to_source(source: str) -> str:
     """Check for missing docstrings in the source code and add them if necessary.
 
@@ -162,7 +213,7 @@ def _check_and_add_docstrings_to_source(source: str) -> str:
 
     return source
 
-# %% ../nbs/Docstring_Generator.ipynb 15
+# %% ../nbs/Docstring_Generator.ipynb 17
 def add_docstring_to_notebook(nb_path: Union[str, Path], version: int = 4):
     """Add docstrings to the source
 
